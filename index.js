d99f5ad338e357234637c3dd7ea561e85020c324
luqmaan <ldawoodjee@gmail.com>
2020-07-22 23:26:56 -0500

    run script

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const csvParse = require('csv-parse/lib/sync')
-const csvGenerate = require('csv-generate')
-const transform = require('stream-transform')
-const _ = require("lodash");
-const stringify = require('csv-stringify')
-
 const fs = require('fs').promises;
+
+const _ = require("lodash");
+const csvParse = require('csv-parse/lib/sync')
+const csvStringify = require('csv-stringify')
 const { Client, Status } = require("@googlemaps/google-maps-services-js");
-const { add } = require('lodash');
 
-const client = new Client({
-});
-
-// const url = require('url');
-// url.fileURLToPath(url)
-// url.pathToFileURL(path)
-
-// list of unique addresses
+// Adds business name, place_id, from Google Maps to complaints.csv 
 // map of address to geocode results
 // map of place_id to place data
 // download and save geocode results
@@ -23,8 +13,15 @@ const client = new Client({
 // download and save place results
 // loop through each row, add place_id, name, types, websites, phone number, business status, user_ratings_total, rating, 
 
+
+const client = new Client({});
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function base64(string) {
+    return Buffer.from(string).toString('base64');
 }
 
 async function writeJSON(path, data) {
@@ -51,25 +48,17 @@ async function getPlace(placeID) {
     }
 
     console.log(`Querying for ${placeID}`)
-
     const details = await client.placeDetails({
         params: {
             place_id: placeID,
             key: process.env.GOOGLE_MAPS_API_KEY,
         },
     });
-
-    await sleep(10);
-
     const result = details.data.result;
 
     await writeJSON(path, result);
-
+    await sleep(3);
     return result;
-}
-
-function base64(string) {
-    return Buffer.from(string).toString('base64');
 }
 
 async function getGeocode(address) {
@@ -82,8 +71,8 @@ async function getGeocode(address) {
     if (existingData != null) {
         return existingData[0];
     }
-    console.log(`Querying for ${address}`)
 
+    console.log(`Querying for ${address}`)
     const details = await client.geocode({
         params: {
             address: address,
@@ -94,65 +83,63 @@ async function getGeocode(address) {
             key: process.env.GOOGLE_MAPS_API_KEY,
         },
     });
-
-    await sleep(10);
-
     const results = details.data.results;
 
     await writeJSON(path, results);
-
+    await sleep(3);
     return results[0];
 }
 
 
 async function main() {
-    try {
-        const complaintsRawCSV = await fs.readFile('complaints.csv');
-        const complaintsCSV = csvParse(complaintsRawCSV, {
-            columns: true,
-            skip_empty_lines: true
-        });
-        const addresses = _.uniq(complaintsCSV.map(c => c.ADDRESS));
+    const complaintsRawCSV = await fs.readFile('complaints.csv');
+    const complaintsCSV = csvParse(complaintsRawCSV, {
+        columns: true,
+        skip_empty_lines: true
+    });
+    const addresses = _.uniq(complaintsCSV.map(c => c.ADDRESS));
 
-        const addressToGeocode = {};
-        const geocodeToPlace = {};
-        const addressToPlace = {};
+    const addressToGeocode = {};
+    const geocodeToPlace = {};
+    const addressToPlace = {};
 
-        for (const address of addresses) {
-            const geocode = await getGeocode(address);
-            addressToGeocode[address] = geocode;
-            if (geocode == null) {
-                console.error('Could not geocode address', address);
-                continue;
-            }
-            const place = await getPlace(geocode.place_id)
-            geocodeToPlace[place] = geocode;
-            addressToPlace[address] = place;
+    let i = 0;
+    for (const address of addresses) {
+        // console.log(`${i++} of ${addresses.length}`);
+        const geocode = await getGeocode(address);
+        addressToGeocode[address] = geocode;
+        if (geocode == null) {
+            console.error('Could not geocode address', address);
+            continue;
         }
-
-        for (complaint of complaintsCSV) {
-            const place =  addressToPlace[complaint.ADDRESS];
-            if (place != null) {
-                complaint.google_place_id = place.place_id;
-                complaint.google_url = place.url;
-                complaint.google_formatted_address = place.formatted_address;
-                complaint.google_user_ratings_total = place.user_ratings_total;
-                complaint.google_website = place.website;
-                complaint.google_types = place.types;
-                complaint.google_rating = place.rating;
-                complaint.google_business_status = place.business_status;
-            }
-        }
-
-        stringify(complaintsCSV, {header: true}, function(err, output){
-            fs.writeFile('complaints_augmented.csv', output);
-          });
-
-    } catch (err) {
-        console.error(err);
+        const place = await getPlace(geocode.place_id)
+        geocodeToPlace[place] = geocode;
+        addressToPlace[address] = place;
     }
+
+
+    for (complaint of complaintsCSV) {
+        const place = addressToPlace[complaint.ADDRESS];
+        if (place != null) {
+            complaint.google_place_id = place.place_id;
+            complaint.google_url = place.url;
+            complaint.google_formatted_address = place.formatted_address;
+            complaint.google_user_ratings_total = place.user_ratings_total;
+            complaint.google_website = place.website;
+            complaint.google_types = place.types;
+            complaint.google_rating = place.rating;
+            complaint.google_business_status = place.business_status;
+        }
+    }
+
+    csvStringify(complaintsCSV, { header: true }, function (err, output) {
+        fs.writeFile('complaints_augmented.csv', output);
+    });
 
 }
 
-main();
-
+try {
+    main();
+} catch (err) {
+    console.error(err);
+}
